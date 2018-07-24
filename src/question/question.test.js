b9bc189f8c600c195fda55953d9b35f5bb6415e9
SandraMesples <sandra.mesples@gmail.com>
2018-07-24 17:45:28 +0200

    ecriture des ebauches de test

--- src/question/question.test.js
+++ src/question/question.test.js
@@ -46,7 +46,7 @@ describe('CRUD /questions', () => {
     });
   });
 
-  describe('Read', () => {
+  describe('Read', () => { // read one
     it('Should get a question and respond 200', (done) => {
       const question = {
         theme: 'geographie',
@@ -74,19 +74,103 @@ describe('CRUD /questions', () => {
     });
 
     it.skip('Should get all questions and respond 200', (done) => {
-      // done(new Error('Not Implmented'));
+        const questionA = {
+          theme: 'maman',
+          question: 'manger?',
+          responses: ['riz', 'pates', 'pizza', 'patate'],
+          response: 'sushi',
+        };
+        const questionB = {
+          theme: 'blalbla',
+          question: 'blobloblo ?',
+          responses: ['aa', 'bb', 'jj', 'aaz'],
+          response: 'oui',
+        };
+
+        Question.create(questionA,questionB) //creation de la question A et B
+          .then((createdQuestion) => {
+            const request = {
+              method: 'GET',
+              url: `/questions/${createdQuestion}`, // plus de id pour ne pas cibler une seule question
+            };
+            const expectedResponseBody = {
+              id: createdQuestion,
+              ...question,
+            };
+            return server.inject(request).then((response) => {
+              expect(response.statusCode).to.be.equal(200); // quand l'on arrive à lire toutes les questions
+              expect(response.result).to.include(expectedResponseBody); // les questions apparaissent
+              done();
+            });
+          })
+          .catch(done);
     });
   });
 
   describe.skip('Update', () => {
     it('Should update a question and respond 200', (done) => {
-      // done(new Error('Not Implmented'));
+      const request = { // on post une question
+        method: 'POST',
+        url: '/questions',
+        payload: {
+          theme: 'geographie',
+          question: 'Quelle est la capitale de la France',
+          responses: ['Bayonne', 'Toulouse', 'Bordeaux', 'Londres'],
+          response: 'Paris',
+        },
+      };
+          const request = { // on la modifie
+            method: 'PUT',
+            url: `/questions/${createdQuestion.id}`, // on sait pas trop quelle cela modifie
+            payload: {
+              theme: 'geographie',
+              question: 'Quelle est la capitale de la France',
+              responses: ['Tokyo', 'Toulouse', 'Bordeaux', 'Londres'],
+              response: 'Paris',
+            },
+          };
+    
+          const expectedResponseBody = { // on attend que la modif soie prise en compte
+            theme: 'geographie',
+            question: 'Quelle est la capitale de la France',
+            responses: ['Tokyo', 'Toulouse', 'Bordeaux', 'Londres'],
+            response: 'Paris',
+          };
+    
+          server
+            .inject(request)
+            .then((response) => {
+              expect(response.statusCode).to.be.equal(200);
+              expect(response.result).to.include('id');
+              expect(response.result).to.include(expectedResponseBody);
+              done();
+            })
+            .catch(done);
+        });
+      });
     });
-  });
 
   describe.skip('Destroy', () => {
     it('Should destroy a question and respond 200', (done) => {
-      // done(new Error('Not Implmented'));
+      const question = {
+        theme: 'geographie',
+        question: 'Quelle est la capitale de la France',
+        responses: ['Bayonne', 'Toulouse', 'Bordeaux', 'Londres'],
+        response: 'Paris',
+      };
+      Question.destroy(question)
+        .then((destroyQuestion) => {
+          const request = {
+            method: 'DELETE',
+            url: `/questions/${destroyQuestion.id}`,
+          };
+
+          return server.inject(request).then((response) => {
+            expect(response.statusCode).to.be.equal(200); // on attend que la reponse et 200
+            expect(response.result).to.be.equal(null || undefined); // on attend que l'objet que l'on a detruit soit null ou undefined
+            done();
+
     });
   });
+});
 });
