6ef1af132ad1ff84b7f29df2520f33d2cf11e18e
SandraMesples <sandra.mesples@gmail.com>
2018-08-06 19:07:42 +0200

    ecriture des test de getQuestions et pickQuestions plus implementation

--- src/tour/tour.controller.js
+++ src/tour/tour.controller.js
@@ -1,16 +1,22 @@
 const Question = require('./../question/question.model.js');
+const laQuestion = require('./../question/question.controller.js');
 
-
-module.exports = { getQuestion, displayQuestion, resolveQuestion };
+module.exports = {
+  getQuestion,
+  displayQuestion,
+  resolveQuestion,
+  getQuestions,
+  pickQuestion,
+};
 function getQuestion() {
   return new Promise((resolve, reject) => {
     Question.findOne({}).then((question) => {
       const answers = question.responses;
       const answerTrue = question.response;
       answers.push(answerTrue);
-      answers.sort(function () {
+      answers.sort(function() {
         return 0.5 - Math.random(); //un sort qui prend en paramètre entre -0.5 et 0.5
-      });                           //donc random à chaque élèment
+      }); //donc random à chaque élèment
       const theQuestion = {
         id: question.id,
         theme: question.theme,
@@ -27,8 +33,7 @@ function displayQuestion(id) {
   return new Promise((resolve, reject) => {
     getQuestion().then((theQuestion) => {
       //id a ajouter en parametre de la fonction des la modif de getQuestion()
-      console.log('[' + theQuestion.theme + '] ' +
-        theQuestion.question);
+      console.log('[' + theQuestion.theme + '] ' + theQuestion.question);
       const answers = theQuestion.answers;
       let i = 1;
       answers.forEach((answer) => {
@@ -36,8 +41,8 @@ function displayQuestion(id) {
         i++;
       });
       resolve();
-    })
-  })
+    });
+  });
 }
 
 function resolveQuestion(id, reponseUser) {
@@ -46,16 +51,48 @@ function resolveQuestion(id, reponseUser) {
       //id a ajouter en parametre de la fonction des la modif de getQuestion()
       if (theQuestion.correctAnswer === theQuestion.answers[reponseUser - 1]) {
         resolve(true);
-      }
-      else {
+      } else {
         reject(false);
       }
-    })
-  })
+    });
+  });
 }
 
+function getQuestions() {
+  return new Promise((resolve, reject) => {
+    Question.findAll({})
+      .then((listQuestions) => {
+        let i = 0;
+        let allQuestions = [];
+        listQuestions.forEach((theQuestion) => {
+          allQuestions[i] = {
+            id: theQuestion.id,
+            theme: theQuestion.theme,
+            isAlreadyUsed: false,
+          };
+          i++;
+        });
+        resolve(allQuestions);
+      })
+      .catch((error) => reject(error));
+  });
+}
 
-//Getquestions(theme)
+function pickQuestion(listQuestions, theme) {
+  return new Promise((resolve, reject) => {
+    let chosenQuestion = [];
+    listQuestions.forEach((theQuestion) => {
+      if (theQuestion.theme === theme && theQuestion.isAlreadyUsed === false) {
+        chosenQuestion.push(theQuestion.id);
+      }
+    });
+    let i = Math.floor(Math.random() * chosenQuestion.length);
+    resolve(chosenQuestion[i]);
+    //reject(error);
+  });
+}
+
+//Getquestions()
 //pickQuestion(questions)
 //GetQuestion(id)
 //afficherQuestion(id)
--- src/tour/tour.test.js
+++ src/tour/tour.test.js
@@ -6,9 +6,10 @@ const getNewQuestions = require('./tour.helper.js').getNewQuestions;
 const getQuestion = require('./tour.controller.js').getQuestion;
 const displayQuestion = require('./tour.controller.js').displayQuestion;
 const resolveQuestion = require('./tour.controller.js').resolveQuestion;
+const getQuestions = require('./tour.controller.js').getQuestions;
+const pickQuestion = require('./tour.controller.js').pickQuestion;
 
 let readline = require('readline');
-
 
 describe('Afficher et répondre à une question', () => {
   beforeEach((done) => {
@@ -20,61 +21,110 @@ describe('Afficher et répondre à une question', () => {
       .then(() => {
         console.info('Questions deleted');
         getNewQuestions(5).then((questions) => {
-          Promise.all(questions.map((question) => {
-            const theQuestion = {
-              theme: question.category,
-              question: question.question,
-              response: question.correct_answer,
-              responses: question.incorrect_answers,
-            };
-            return Question.create(theQuestion);
-          }))
-            .then(() => {
-              console.info("Database filled")
-              done();
+          Promise.all(
+            questions.map((question) => {
+              const theQuestion = {
+                theme: 'MON THEME', //question.category,
+                question: question.question,
+                response: question.correct_answer,
+                responses: question.incorrect_answers,
+              };
+              return Question.create(theQuestion);
             })
+          ).then(() => {
+            console.info('Database filled');
+            done();
+          });
         });
       })
       .catch(done);
   });
 
   it('Should display a question', (done) => {
-    getQuestion().then((theQuestion) => {
-      expect(theQuestion).to.only.include([
-        'id',
-        'theme',
-        'question',
-        'correctAnswer',
-        'answers',
-      ]);
-      expect(theQuestion.answers)
-        .to.be.an.array()
-        .and.to.include(theQuestion.correctAnswer);
-      expect(theQuestion.answers.length).to.be.at.least(2);
-      done();
-
-    }).catch(done);
-
+    getQuestion()
+      .then((theQuestion) => {
+        expect(theQuestion).to.only.include([
+          'id',
+          'theme',
+          'question',
+          'correctAnswer',
+          'answers',
+        ]);
+        expect(theQuestion.answers)
+          .to.be.an.array()
+          .and.to.include(theQuestion.correctAnswer);
+        expect(theQuestion.answers.length).to.be.at.least(2);
+        done();
+      })
+      .catch(done);
   });
 
   it('Should say to us if the answer is correct or not ', (done) => {
-    getQuestion().then((theQuestion) => {
-      const correctAnswer = theQuestion.correctAnswer;
-      let indexBonneReponse = 0;
-      let answersOfQuestion = theQuestion.answers;
-      for (let i = 0; i < answersOfQuestion.length; i++) {
-        if (correctAnswer === answersOfQuestion[i]) {
-          indexBonneReponse = i;
+    getQuestion()
+      .then((theQuestion) => {
+        const correctAnswer = theQuestion.correctAnswer;
+        let indexBonneReponse = 0;
+        let answersOfQuestion = theQuestion.answers;
+        for (let i = 0; i < answersOfQuestion.length; i++) {
+          if (correctAnswer === answersOfQuestion[i]) {
+            indexBonneReponse = i;
+          }
         }
-      }
-      resolveQuestion(theQuestion.id, indexBonneReponse + 1).then((isCorrect) => {
-        expect(isCorrect).to.be.true();
-        resolveQuestion(theQuestion.id, 7).then(done).catch((isCorrect) => {
-          expect(isCorrect).to.be.false();
-          done();
-        })
-      }).catch(done);
-    })
+        resolveQuestion(theQuestion.id, indexBonneReponse + 1)
+          .then((isCorrect) => {
+            expect(isCorrect).to.be.true();
+            resolveQuestion(theQuestion.id, 7)
+              .then(done)
+              .catch((isCorrect) => {
+                expect(isCorrect).to.be.false();
+                done();
+              });
+          })
+          .catch(done);
+      })
       .catch(done);
-  })
+  });
+});
+it('Should get all the questions of the database and put it on an array', (done) => {
+  getQuestions()
+    .then((listQuestions) => {
+      expect(listQuestions).to.be.an.array();
+      expect(listQuestions.length).to.be.at.least(2);
+
+      expect(listQuestions[0].id).to.exist();
+      expect(listQuestions[0].id).to.be.a.string();
+
+      expect(listQuestions[0].theme).to.exist();
+      expect(listQuestions[0].theme).to.be.a.string();
+
+      expect(listQuestions[0].isAlreadyUsed).to.exist();
+      expect(listQuestions[0].isAlreadyUsed).to.be.a.boolean();
+
+      done();
+    })
+    .catch(done);
+});
+
+it('Sould pick one question', (done) => {
+  getQuestions()
+    .then((listQuestions) => {
+      pickQuestion(listQuestions, 'MON THEME') // MON THEME sera modifié ulterieurement en fonction du tour
+        .then((chosenQuestion) => {
+          expect(chosenQuestion).to.be.a.string();
+          let i = 0;
+          while ( i < listQuestions.length && listQuestions[i].id != chosenQuestion) {
+            i++;
+          }
+          if(i != listQuestions.length -1)
+          {
+            expect(listQuestions[i].isAlreadyUsed).to.be.false();
+            expect(listQuestions[i].theme).to.be.equal('MON THEME');
+            done();
+          }
+            
+          
+        })
+        .catch(done);
+    })
+    .catch(done);
 });
