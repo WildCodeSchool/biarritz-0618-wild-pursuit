6c38353c00b26c99001e66ec7c17a9b22b37971c
SandraMesples <sandra.mesples@gmail.com>
2018-08-14 13:57:12 +0200

    implementation composent bouton

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Button from './Button.jsx';
+//import De from './De.jsx'
+//import React from 'react'
+import ReactDOM from 'react-dom'
+//import { AppContainer } from 'react-hot-loader';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Button />
       </div>
     );
   }
+  
+  
 }
+const render = (Component) => {
+  ReactDOM.render(
+    //<AppContainer>
+      <Component/>,
+    //</AppContainer>
+    
+    document.getElementById('App')
+  );
+};
+
+render();
+
+
+// Hot Module Replacement API
+// if (module.hot) {
+//   module.hot.accept('./TestApp', () => {
+//     const NextApp = require('./TestApp');
+//     render(NextApp)
+//   });
+// }
+
 
 export default App;
