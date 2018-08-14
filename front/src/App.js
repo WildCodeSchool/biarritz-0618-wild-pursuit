a62cd08fab968375394bf861131957bf19ad7808
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-14 15:37:36 +0200

    implement CountDown component

--- front/src/App.js
+++ front/src/App.js
@@ -5,16 +5,18 @@ import './App.css';
 import Ranking from './ranking/Ranking.jsx';
 import Social from './social/Social.jsx';
 import Die from './Die.js';
+import CountDown from './countDown/CountDown.jsx';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Die />
 				<h1>Wild Pursuit</h1>
 				<h2>A la recherche de la quête perdue</h2>
 				<Ranking />
 				<Social />
-				<Die />
+				<CountDown seconds="65" />
 			</div>
 		);
 	}
---
+++ front/src/countDown/CountDown.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class CountDown extends Component {
+	constructor(props) {
+		super();
+		this.state = { ...props, time: { m: 0, s: 0 } };
+		this._timer = 0;
+		this.startTimer = this.startTimer.bind(this);
+		this.countDown = this.countDown.bind(this);
+		this.display = this.display.bind(this);
+	}
+
+	componentDidMount() {
+		this.startTimer();
+	}
+	startTimer() {
+		if (this._timer == 0) {
+			this._timer = setInterval(this.countDown, 1000);
+		}
+	}
+
+	secondsToTime(secs) {
+		let divisor_for_minutes = secs % (60 * 60);
+		let minutes = Math.floor(divisor_for_minutes / 60);
+
+		let divisor_for_seconds = divisor_for_minutes % 60;
+		let seconds = Math.ceil(divisor_for_seconds);
+
+		let obj = {
+			m: minutes,
+			s: seconds,
+		};
+		return obj;
+	}
+	countDown() {
+		// Remove one second, set state so a re-render happens.
+		let seconds = this.state.seconds - 1;
+		this.setState({
+			time: this.secondsToTime(seconds),
+			seconds: seconds,
+		});
+
+		// Check if we're at zero.
+		if (seconds == 0) {
+			clearInterval(this._timer);
+			alert('Le compte à rebours est fini !');
+		}
+	}
+	display() {
+		let displayTime = '';
+		if (this.state.time.m != 0) {
+			displayTime = displayTime + this.state.time.m + ' Min';
+		}
+		displayTime = displayTime + this.state.time.s + ' Sec';
+		return displayTime;
+	}
+	render() {
+		return <div>⏱ {this.display()}</div>;
+	}
+}
+
+export default CountDown;
