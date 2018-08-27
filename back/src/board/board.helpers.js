f27754994bafb467f3cacdd7c6d410b6a80afed7
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-27 16:56:16 +0200

    passing test for createBoxes()

--- back/src/board/board.helpers.js
+++ back/src/board/board.helpers.js
@@ -24,4 +24,28 @@ function createBox(id, posX, posY, type) {
   return aBox;
 }
 
-module.exports = { createBox };
+function createBoxes(nbCat) {
+  let theBoxes = [];
+  if (nbCat >= 4) {
+    for (let i = 0; i <= 2 * nbCat * nbCat; i++) {
+      let type = 'default';
+
+      if (i === 0) {
+        type = 'center';
+      } else if (!(i % nbCat) && i <= nbCat * nbCat) {
+        type = 'cheese';
+      } else if (i > nbCat * nbCat && (i % nbCat === 2 || i % nbCat === 5)) {
+        //ne marche que pour la génération à 6 catégories pour le moment
+        //to fix : prévoir les scénarios de génération des cases replay pour nbCat [4,inf]
+        type = 'replay';
+      }
+
+      theBoxes[i] = createBox(i, 0, 0, type);
+    }
+  } else {
+    console.log('Le plateau ne peux pas avoir moins de 4 catégories');
+  }
+  return theBoxes;
+}
+
+module.exports = { createBox, createBoxes };
