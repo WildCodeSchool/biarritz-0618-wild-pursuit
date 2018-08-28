629fcf14b2ba961a61b418c89c3b5338b25e8070
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-28 18:33:57 +0200

    PseudoCode Tour

---
+++ back/pseudoCode/Tour.js
@@ -0,0 +1,32 @@
+Décompte = 60
+Décompte = lancerDecompte(Décompte)
+resultat = lancerDes(6)
+position = recupérerPosition(joueur)
+afficherCasesPossible(position, resultat)
+position = deplacerJouer(choixUtilisateur())
+theme = récupérerTheme(position)
+question = choisirQuestion(theme)
+afficherQuestion(question)
+Si (Décompte > 15)
+{
+    Décompte = 15
+}
+
+Réponse = ReponseUtilisateur(ReponseUtilisateur)
+
+Si (Décompte == 0) {
+    Réponse = False
+}
+Retourner Réponse
+
+####
+
+Fonction lancerDecompte(nbrSecondes) // Tous les 1 secondes, on décrémente nbrSecondes
+Fonction lancerDes(nbFace) // Retourne un entier aléatoire entre 1 et nbFaces
+Fonction RecupérerPosition(Joueur) // Retourne l'id de la case sur lequel se trouve le joueur
+Fonction afficherCasesPossible(position, resultat) // Retourne un tableau de case possibles à distance de la position actuelle
+Fonction deplacerJoueur(position) // déplacer le joueur a une position donnée, renvoie la nouvelle position
+Fonction recupérerThème(case) // rénvoie le thème de case passé en paramètre
+Fonction choisirQuestion(theme) // choisi un question dans le thème et la passe a "utilisé"
+Fonction afficherQuestion(question) // renvoie la question formatée
+Fonction ReponseUtilisateur(réponse) // test si la réponse est bonne (renvoie tru si c'est le cas, false sinon)
