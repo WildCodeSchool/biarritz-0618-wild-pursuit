597a997adf57cdd2d069985fd04ca71baf2c5fff
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-28 17:57:22 +0200

    PseudoCode Partie

--- back/pseudoCode/Partie.js
+++ back/pseudoCode/Partie.js
@@ -1,57 +1,15 @@
-    serveur return listGames;
-    serveur return "creer partie";
-
-    if (client onClick listGames[i] && nbrClients = nbrClientsMax) {
-        countDown(timeMax);
-        serveur return plateau;
-        serveur place pions sur caseMiddle;
-        serveur chooseFirst();
-
-        while (clientAdmin === connecté || finDuJeu === false) {
-            serveur round();
-
-
-            if (client resolvedQuestion(true)) {
-                if (caseCamembert) {
-                    client.camembert++;
-                }
-                else if (client.camembert === 6 && caseCenter) {
-                    serveur endGame();
-                }
-            }else {
-                serveur changeClient();
-            }
+joueurCourant = selectionnerJoueurRandom(tabJoueurs)
+FinDePartie = False
+Tant que (FinDePartie == False) {
+    leResultatDuTour = tour(joueurCourant)
+    Si (leResultatDuTour == true) {
+        Si (joueurCourant.case == "camembert"){
+            joueurCourant.nbCamemberts++
+        } Sinon Si (joueurCourant.case == "centre" && joueurCourant.nbCamemberts == nbCatégories) {
+            FinDePartie = True
         }
-    }
-    else if (client onClick "créer partie") {
-    serveur return ecranChoix;
-    client chooseNbrClientsMax();
-    client chooseTimeMax();
-    client createGame();
-
-    if(nbrClients = nbrClientsMax || nbrClients >= 2){
-        client can playGame();
-    }
-
-    countDown(timeMax);
-    serveur return plateau;
-    serveur place pions sur caseMiddle;
-    serveur chooseFirst();
-
-    while (clientAdmin === connecté || finDuJeu === false) {
-        serveur round();
-
-
-        if (client resolvedQuestion(true)) {
-            if (caseCamembert) {
-                client.camembert++;
-            }
-            else if (client.camembert === 6 && caseCenter) {
-                serveur endGame();
-            }
-        }else {
-            serveur changeClient();
-        }
+        joueurCourant = joueurCourant
+    } Sinon {
+        joueurCourant = tabJoueurs[joueurCourant.index + 1]
     }
 }
-
