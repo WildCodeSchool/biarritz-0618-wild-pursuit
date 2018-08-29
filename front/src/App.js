54497d4b3dac30dd1fa8df91b7bdd77c71b048bb
karolyn <c_fayolle@live.fr>
2018-08-29 13:31:17 +0200

    implementing StopGame Component

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Ranking from './ranking/Ranking.jsx';
-import Social from './social/Social.jsx';
-import Dice from './dice/Dice.jsx';
-import CountDown from './countDown/CountDown.jsx';
-import Plateau from './Plateau.jsx';
-import CreateGame from './createGame/CreateGame.jsx';
-import Lobby from './Lobby.jsx';
-import CountDownToGame from './countDownToGame/CountDownToGame.jsx';
-import WaitingForPlayers from './waitingForPlayers/WaitingForPlayers.jsx';
-import Question from './question/Question.jsx';
-import Popup from './popup/Popup.jsx';
+import Ranking from "./ranking/Ranking.jsx";
+import Social from "./social/Social.jsx";
+import Dice from "./dice/Dice.jsx";
+import CountDown from "./countDown/CountDown.jsx";
+import Plateau from "./Plateau.jsx";
+import CreateGame from "./createGame/CreateGame.jsx";
+import Lobby from "./Lobby.jsx";
+import CountDownToGame from "./countDownToGame/CountDownToGame.jsx";
+import WaitingForPlayers from "./waitingForPlayers/WaitingForPlayers.jsx";
+import Question from "./question/Question.jsx";
+import Popup from "./popup/Popup.jsx";
+import StopGame from "./stopgame/StopGame.jsx";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Dice 
-				result={3}
-				/>
-				<h1>Wild Pursuit</h1>
-				<h2>A la recherche de la quête perdue</h2>
-				<Lobby />
-				<Ranking />
-				<Social />
-				<Plateau />
-				<Popup comp={<CreateGame />} />
-				<Popup comp={<CountDownToGame countDown={10} />} />
-				<Popup comp={<WaitingForPlayers maxPLayers={8} />} />
-				<Popup comp={<Question />} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Dice result={3} />
+        <h1>Wild Pursuit</h1>
+        <h2>A la recherche de la quête perdue</h2>
+        <Lobby />
+        <Ranking />
+        <Social />
+        <Plateau />
+        <Popup comp={<CreateGame />} />
+        <Popup comp={<CountDownToGame countDown={10} />} />
+        <Popup comp={<WaitingForPlayers maxPLayers={8} />} />
+        <Popup comp={<Question />} />
+        <Popup comp={<StopGame />} />
+      </div>
+    );
+  }
 }
 
 export default App;
