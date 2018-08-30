bac8ee9c7be3acda7faabbd74f26d0ddf6fe823d
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-30 07:34:19 +0200

    Cleaning App of unnecessary components

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import CountDown from "./countDown/CountDown.jsx";
-
 import Lobby from "./Lobby.jsx";
 import Game from "./game/Game.jsx";
-import NormalBox from "./normalBox/NormalBox.jsx";
-import CheeseBox from "./cheeseBox/CheeseBox.jsx";
-import ReplayBox from "./replayBox/ReplayBox.jsx";
-import CenterBox from "./centerBox/CenterBox.jsx";
 
 class App extends Component {
   render() {
@@ -18,46 +12,6 @@ class App extends Component {
         <h2>A la recherche de la quête perdue</h2>
         <Lobby />
         <Game id={2156} />
-        <NormalBox
-          objet={{
-            id: 27,
-            coord: [78, 67],
-            categorie: {
-              nom: "Géographie",
-              couleur: { nom: "Rouge", code: "#FF0000" }
-            }
-          }}
-        />
-        <CheeseBox
-          cheeseObjet={{
-            id: 34,
-            coord: [11, 30],
-            categorie: {
-              nom: "Histoire",
-              couleur: { nom: "Noir", code: "#FFFFFF" }
-            }
-          }}
-        />
-        <ReplayBox
-          replayObjet={{
-            id: 22,
-            coord: [14, 71],
-            categorie: {
-              nom: "Loisir",
-              couleur: { nom: "Rose", code: "#FFFFFF" }
-            }
-          }}
-        />
-        <CenterBox
-          centerObjet={{
-            id: 82,
-            coord: [0, 0],
-            categorie: {
-              nom: "Loisir",
-              couleur: { nom: "Bleu", code: "#FFFFFF" }
-            }
-          }}
-        />
       </div>
     );
   }
