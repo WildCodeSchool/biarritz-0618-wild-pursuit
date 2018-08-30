74b4ee90b921c23ae2cdb7bbaf799261c674d160
MaXo2 <yann.michel.maxo2@gmail.com>
2018-08-30 07:31:29 +0200

    implementing Boxes Component on Board Component

--- front/src/board/Board.jsx
+++ front/src/board/Board.jsx
@@ -1,25 +1,55 @@
 import React, { Component } from "react";
 
+import NormalBox from "./../normalBox/NormalBox.jsx";
+import CheeseBox from "./../cheeseBox/CheeseBox.jsx";
+import ReplayBox from "./../replayBox/ReplayBox.jsx";
+import CenterBox from "./../centerBox/CenterBox.jsx";
+
 export default class Board extends Component {
-  constructor() {
-    super();
-    this.cases = [
-      { caseNorm: "Case normale" },
-      { caseCam: "Case camembert" },
-      { caseCent: "Case centrale" },
-      { caseReplay: "Case rejouer" }
-    ];
-  }
-
-  generateDiv() {
-    let plateau = [];
-    for (let laCase = 0; laCase < this.cases.length; laCase++) {
-      plateau.push(<div key={laCase}>{laCase}</div>);
-    }
-    return plateau;
-  }
-
   render() {
-    return <div>{this.generateDiv()}</div>;
+    return (
+      <div>
+        <NormalBox
+          objet={{
+            id: 27,
+            coord: [78, 67],
+            categorie: {
+              nom: "Géographie",
+              couleur: { nom: "Rouge", code: "#FF0000" }
+            }
+          }}
+        />
+        <CheeseBox
+          cheeseObjet={{
+            id: 34,
+            coord: [11, 30],
+            categorie: {
+              nom: "Histoire",
+              couleur: { nom: "Noir", code: "#FFFFFF" }
+            }
+          }}
+        />
+        <ReplayBox
+          replayObjet={{
+            id: 22,
+            coord: [14, 71],
+            categorie: {
+              nom: "Loisir",
+              couleur: { nom: "Rose", code: "#FFFFFF" }
+            }
+          }}
+        />
+        <CenterBox
+          centerObjet={{
+            id: 82,
+            coord: [0, 0],
+            categorie: {
+              nom: "Loisir",
+              couleur: { nom: "Bleu", code: "#FFFFFF" }
+            }
+          }}
+        />
+      </div>
+    );
   }
 }
