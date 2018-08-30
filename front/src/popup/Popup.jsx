f2de0a3b57a3423e618bd6862dd08f8c4b84d540
SandraMesples <sandra.mesples@gmail.com>
2018-08-30 15:16:32 +0200

    boutton X

--- front/src/popup/Popup.jsx
+++ front/src/popup/Popup.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ButtonMUI from '@material-ui/core/Button';
+//import "./popup.css";
 
 const Popup = ({ comp }) => (
   <div>
+    <div  class='popup'>
+  <ButtonMUI onClick='fermer le popup' >X</ButtonMUI>
+  </div>
     {comp}
-    <div>
-      <ButtonMUI onClick='fermer le popup'>X</ButtonMUI>
-    </div>
   </div>
 );
 
