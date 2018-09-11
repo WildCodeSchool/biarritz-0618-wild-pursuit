53284559caaae998dc602a5a9f630a0a3d6e73b2
MaXo2 <yann.michel.maxo2@gmail.com>
2018-09-11 17:42:04 +0200

    implementing theme for buttons

--- front/src/commun/button/Button.jsx
+++ front/src/commun/button/Button.jsx
@@ -2,9 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import ButtonMUI from "@material-ui/core/Button";
 
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import red from "@material-ui/core/colors/red";
 
-const Button = ({ name, variant, onClick, disabled }) => (
-  <ButtonMUI {...{ variant, onClick, disabled }}>{name}</ButtonMUI>
+const muiTheme = createMuiTheme({
+  palette: {
+    primary: { main: "#f44336" },
+    secondary: {
+      main: "#689f38"
+    }
+  }
+});
+
+const Button = ({
+  name,
+  variant,
+  onClick,
+  disabled,
+  color = "default",
+  disableRipple
+}) => (
+  <MuiThemeProvider theme={muiTheme}>
+    <ButtonMUI {...{ variant, onClick, disabled, color, disableRipple }}>
+      {name}
+    </ButtonMUI>
+  </MuiThemeProvider>
 );
 
 Button.propTypes = {
