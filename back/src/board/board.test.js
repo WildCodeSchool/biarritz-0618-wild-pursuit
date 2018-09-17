611e379cbbba57c05faf3be3c8ccc4b26255cf8c
MaXo2 <yann.michel.maxo2@gmail.com>
2018-09-17 11:50:26 +0200

    Writing tests to implement Themes

--- back/src/board/board.test.js
+++ back/src/board/board.test.js
@@ -11,19 +11,37 @@ const { createBoxes, createPaths } = require('./board.helpers.js');
 const { NormalBoard } = require('./board.class.js');
 
 describe('Generate a board', () => {
-  it('should generate an array of boxes with a pattern', (done) => {
-    const nbCat = 6;
-    const boxes = createBoxes(nbCat);
+  it('Should generate an array of boxes with a pattern', (done) => {
+    const themes = [
+      { name: 'Histoire' },
+      { name: 'Géographie' },
+      { name: 'Science' },
+      { name: 'Télévision' },
+      { name: 'Théatre' },
+      { name: 'Sport' },
+    ];
+    const boxes = createBoxes(themes);
     expect(boxes).to.be.an.array();
-    expect(boxes).to.have.length(nbCat * nbCat * 2 + 1);
+    expect(boxes).to.have.length(themes.length * themes.length * 2 + 1);
     expect(boxes[0], 'Objet').to.be.an.instanceOf(CenterBox);
-    expect(boxes[nbCat], 'Objet').to.be.an.instanceOf(CheeseBox);
-    expect(boxes[nbCat * nbCat + 2], 'Objet').to.be.an.instanceOf(ReplayBox);
+    expect(boxes[themes.length], 'Objet').to.be.an.instanceOf(CheeseBox);
+    expect(
+      boxes[themes.length * themes.length + 2],
+      'Objet'
+    ).to.be.an.instanceOf(ReplayBox);
     done();
   });
-  it('should generate an array of paths (arrays) with a pattern', (done) => {
-    const nbCat = 6;
-    const boxes = createBoxes(nbCat);
+  it('Should generate an array of paths (arrays) with a pattern', (done) => {
+    const themes = [
+      { name: 'Histoire' },
+      { name: 'Géographie' },
+      { name: 'Science' },
+      { name: 'Télévision' },
+      { name: 'Théatre' },
+      { name: 'Sport' },
+    ];
+    let nbCat = themes.length;
+    const boxes = createBoxes(themes);
     const paths = createPaths(nbCat, boxes);
     expect(paths, '#1').to.be.an.array();
     expect(paths, '#2').to.have.length(nbCat * 2 * 2);
@@ -57,54 +75,66 @@ describe('Generate a board', () => {
 
     done();
   });
-  it('should create an instance of NormalBoard', (done) => {
-    const nbCat = 6;
-    const theBoard = new NormalBoard(nbCat);
+  it('Should create an instance of NormalBoard', (done) => {
+    let theBoard = new NormalBoard(6);
+    theBoard
+      .init()
+      .then(() => {
+        expect(theBoard).to.be.an.instanceOf(NormalBoard);
+        expect(theBoard.getNeighbors(theBoard.boxes[4], 6)).to.be.an.array();
 
-    expect(theBoard).to.be.an.instanceOf(NormalBoard);
-    expect(theBoard.getNeighbors(theBoard.boxes[4], 6)).to.be.an.array();
+        let result = [
+          theBoard.boxes[40],
+          theBoard.boxes[69],
+          theBoard.boxes[8],
+          theBoard.boxes[14],
+          theBoard.boxes[20],
+          theBoard.boxes[26],
+          theBoard.boxes[32],
+        ];
+        expect(
+          theBoard.getNeighbors(theBoard.boxes[4], 6),
+          'blop :'
+        ).to.be.equal(result);
 
-    let result = [
-      theBoard.boxes[40],
-      theBoard.boxes[69],
-      theBoard.boxes[8],
-      theBoard.boxes[14],
-      theBoard.boxes[20],
-      theBoard.boxes[26],
-      theBoard.boxes[32],
-    ];
-    expect(theBoard.getNeighbors(theBoard.boxes[4], 6)).to.be.equal(result);
+        result = [theBoard.boxes[3], theBoard.boxes[39], theBoard.boxes[70]];
+        expect(
+          theBoard.getNeighbors(theBoard.boxes[6], 3),
+          'blop :'
+        ).to.be.equal(result);
 
-    result = [theBoard.boxes[3], theBoard.boxes[39], theBoard.boxes[70]];
-    expect(theBoard.getNeighbors(theBoard.boxes[6], 3)).to.be.equal(result);
+        result = [
+          theBoard.boxes[38],
+          theBoard.boxes[71],
+          theBoard.boxes[47],
+          theBoard.boxes[50],
+          theBoard.boxes[53],
+          theBoard.boxes[56],
+          theBoard.boxes[59],
+          theBoard.boxes[62],
+          theBoard.boxes[65],
+          theBoard.boxes[68],
+          theBoard.boxes[1],
+          theBoard.boxes[7],
+          theBoard.boxes[19],
+          theBoard.boxes[25],
+          theBoard.boxes[31],
+          theBoard.boxes[24],
+          theBoard.boxes[9],
+          theBoard.boxes[15],
+          theBoard.boxes[21],
+          theBoard.boxes[27],
+          theBoard.boxes[33],
+          theBoard.boxes[34],
+          theBoard.boxes[65],
+        ];
+        expect(
+          theBoard.getNeighbors(theBoard.boxes[42], 15),
+          'blop :'
+        ).to.be.equal(result);
 
-    result = [
-      theBoard.boxes[38],
-      theBoard.boxes[71],
-      theBoard.boxes[47],
-      theBoard.boxes[50],
-      theBoard.boxes[53],
-      theBoard.boxes[56],
-      theBoard.boxes[59],
-      theBoard.boxes[62],
-      theBoard.boxes[65],
-      theBoard.boxes[68],
-      theBoard.boxes[1],
-      theBoard.boxes[7],
-      theBoard.boxes[19],
-      theBoard.boxes[25],
-      theBoard.boxes[31],
-      theBoard.boxes[24],
-      theBoard.boxes[9],
-      theBoard.boxes[15],
-      theBoard.boxes[21],
-      theBoard.boxes[27],
-      theBoard.boxes[33],
-      theBoard.boxes[34],
-      theBoard.boxes[65],
-    ];
-    expect(theBoard.getNeighbors(theBoard.boxes[42], 15)).to.be.equal(result);
-
-    done();
+        done();
+      })
+      .catch(done);
   });
 });
