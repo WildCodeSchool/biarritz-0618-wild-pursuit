fa2eac7f1fb27932e7a9f6b0ae809ac3f8fbcca1
MaXo2 <yann.michel.maxo2@gmail.com>
2018-09-17 13:17:18 +0200

    Implementing Game controller

--- back/src/game/game.controller.js
+++ back/src/game/game.controller.js
@@ -1,3 +1,6 @@
+const Game = require('./game.class.js');
+const launchDice = require('../dice/dice.controller.js');
+
 function pickAPlayer(players) {
   return players[Math.floor(Math.random() * players.length)];
 }
@@ -7,4 +10,25 @@ function getNextPlayer(player, players) {
   return players[nextPlayerIndex];
 }
 
-module.exports = { pickAPlayer, getNextPlayer };
+function launchGame(nbCat, nbMaxPlayers) {
+  let aGame = new Game(nbCat, nbMaxPlayers);
+  aGame.init().then(() => {
+    aGame.newPlayer();
+    aGame.newPlayer();
+    aGame.newPlayer();
+    aGame.newPlayer();
+    let player = pickAPlayer(aGame.players);
+    console.log('Le joueur qui joue : ' + player.name);
+    let result = launchDice();
+    console.log('Le résultat du dé : ' + result);
+    console.log(
+      aGame.board.getNeighbors(aGame.board.boxes[player.position], result)
+    );
+
+    player.position = 5; //CHOIX UTILISATEUR
+    console.log(player.name + ' se déplace sur la case n°' + player.position);
+    console.log(aGame.board.boxes[player.position]);
+  });
+}
+
+module.exports = { launchGame };
